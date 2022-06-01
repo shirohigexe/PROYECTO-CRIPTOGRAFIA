7bb4cbad85bd30d0166491cdb1312346ef2f1380
Juan Pablo Toro Arenas <jutoroa@unal.edu.co>
2022-06-01 12:28:08 -0500

    Transformacion LIneal

--- 1RA.py
+++ 1RA.py
@@ -1,16 +1,18 @@
+"""
+Programa para generar la llave pública y privada
+"""
+
 from typing import Counter
 import sympy as S 
 import random 
+from pyfinite import ffield      # campo finito
 
-#campo finito
-from pyfinite import ffield
-
-valor_primo=[3, 5, 7]
-valor_campo=random.choice(valor_primo)
-F = ffield.FField(valor_campo) 
+valor_primo     = [3, 5, 7]                     # Posibles valores para escoger el campo
+valor_campo     = random.choice(valor_primo)    # Se escoge el primo para el campo
+F = ffield.FField(valor_campo)                  # Se crea el campo
 #help(ffield.FField)
-print(valor_campo)
-print(F.ShowPolynomial(valor_campo))
+#print(valor_campo)
+#print(F.ShowPolynomial(valor_campo))
 
 
 #tamaño del numero a codificar
@@ -18,9 +20,13 @@ n=6
 
 #matriz aleatoria
 import numpy as np
+
 #crear un array numpy con ceros
-A = np.zeros((n,n))
-print(A)
+transformacion_lineal_0 = np.zeros((n,n))
+print(transformacion_lineal_0)
+a = np.sum(transformacion_lineal_0, axis=0)
+print(a)
+print(a[1])
 #vector de simbolos
 α= S.symbols('α')
 #np.power(x,2)
@@ -28,26 +34,35 @@ ver_simbo=[0, 1, np.power(α,1), np.power(α,2)]
 #ver_simbo=[0, 1, α**1, α**2]
 
 #generacion de matriz aleatoria
-b = (A.tolist())
-contador = len(b)**2
+transformacion_lineal = (transformacion_lineal_0.tolist())
+contador = len(transformacion_lineal)**2
 while contador > 1:
-    for i in b:
+    for i in transformacion_lineal:
         valor_aleatorio = random.choice(ver_simbo)
         indice_cambio = random.randint(0,len(i)-1)
         i[indice_cambio] = valor_aleatorio
     contador -= 1
 
-matriz1=np.array(b)
-#print(np.array(b))
+# Convertimos el arreglo en una matriz de numpy
+transformacion_lineal = np.array(transformacion_lineal)
+print(transformacion_lineal)
 
 #variables x1 a xn
-x_inicial=[]
+x_inicial=[]                                    # Arreglo de varibles iniciales
 for i in range(n):
-    x_inicial.append(S.symbols("x"+str(i)))
+    x_inicial.append(S.symbols("x"+str(i)))     # Creamos las xn variables
+x_inicial = np.transpose(x_inicial)             # Creamos el vector X
 print(x_inicial)
 
 #transformada
-
+print("*************RESULTADO**************")
+print(transformacion_lineal*x_inicial)
+print("*************RESULTADO**************")
+print(np.transpose(transformacion_lineal*x_inicial))
+print("POLIX")
+print(np.sum(np.transpose(transformacion_lineal*x_inicial), axis=0))
+print("DOT PRODUCT")
+print(transformacion_lineal.dot(x_inicial))
 
 
 
