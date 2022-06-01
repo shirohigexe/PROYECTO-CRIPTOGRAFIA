67e6b9f1bca549276e230ce91a85e338b3866dce
Cabroc <104835411+Cabroc@users.noreply.github.com>
2022-05-31 21:30:26 -0500

    Update 1RA.py

--- 1RA.py
+++ 1RA.py
@@ -7,16 +7,32 @@ valor_primo=[3, 5, 7]
 valor_campo=random.choice(valor_primo)
 F = ffield.FField(valor_campo) 
 #help(ffield.FField)
-print(len(F))
+F.ShowPolynomial
+
 
 #matriz aleatoria
 import numpy as np
  #crear un array numpy con ceros
-a = np.zeros(valor_campo-1)
-print(a)
+A = np.zeros((valor_campo-1,valor_campo-1))
+print(A)
 #vector de simbolos
-ver_simbo=[0, 1, x, x**2]
-#arreglo en la matrix
+x= S.symbols('x')
+#np.power(x,2)
+ver_simbo=[0, 1, np.power(x,1), np.power(x,2)]
+#ver_simbo=[0, 1, x**1, x**2]
+
+
+b = (A.tolist())
+contador = len(b)**2
+while contador > 1:
+    for i in b:
+        valor_aleatorio = random.choice(ver_simbo)
+        indice_cambio = random.randint(0,len(i)-1)
+        i[indice_cambio] = valor_aleatorio
+    contador -= 1
+
+print(b)
+
 
 
 
