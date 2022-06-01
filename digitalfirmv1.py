8dbae70d9b5a16dcc86b633a3fa0f06dfad92a52
Juan Pablo Toro Arenas <jutoroa@unal.edu.co>
2022-06-01 09:37:31 -0500

    DigitalFirmV2

--- digitalfirmv1.py
+++ digitalfirmv1.py
@@ -1,6 +1,20 @@
-import numpy as np
-  
-  
-array = np.random.randint(2, size=(5, 5, 1))
-print(array)
-print(1)
+"""
+from sympy import *
+import itertools
+D = (3, 4, 2, 3)
+a = symarray("a", D)
+x = symarray("x", len(D))
+prod_iterator = itertools.product(*map(range, D))
+result = Add(*[a[p]*Mul(*[v**d for v, d in zip(x, p)]) for p in prod_iterator])
+print(result)
+"""
+
+import sympy as sympy
+
+n = 6
+vector = []
+
+for i in range(n):
+    vector.append(sympy.symbols("x"+str(i)))
+
+print(vector)
