4ad32b54710413a88f17b7fd72b00ac4758e3afc
shirohigexe <99839298+shirohigexe@users.noreply.github.com>
2022-05-31 21:44:08 -0500

    .

--- 1RA.py
+++ 1RA.py
@@ -3,23 +3,25 @@ import random
 
 #campo finito
 from pyfinite import ffield
+
 valor_primo=[3, 5, 7]
 valor_campo=random.choice(valor_primo)
 F = ffield.FField(valor_campo) 
 #help(ffield.FField)
-F.ShowPolynomial
+print(valor_campo)
+print(F.ShowPolynomial(valor_campo))
+
 
 
 #matriz aleatoria
 import numpy as np
- #crear un array numpy con ceros
+#crear un array numpy con ceros
 A = np.zeros((valor_campo-1,valor_campo-1))
 print(A)
 #vector de simbolos
 x= S.symbols('x')
 #np.power(x,2)
 ver_simbo=[0, 1, np.power(x,1), np.power(x,2)]
-#ver_simbo=[0, 1, x**1, x**2]
 
 
 b = (A.tolist())
@@ -31,7 +33,7 @@ while contador > 1:
         i[indice_cambio] = valor_aleatorio
     contador -= 1
 
-print(b)
+print(np.array(b))
 
 
 
